c3405dfd57cd1c4730292c4a01e788823584e8d5
Tad Sekeres <twsekeres@gmail.com>
2021-01-30 22:58:08 -0600

    finished JS functions

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+console.log("Ho Ender");
 
 const projects = [
   {
@@ -8,7 +9,7 @@ const projects = [
     available: true,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  }
+  },
   {
     title: "Cool Project", 
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
@@ -17,13 +18,13 @@ const projects = [
     available: true,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  }
+  },
   {
     title: "Cool Project", 
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
+    available: false,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
   }
@@ -36,22 +37,25 @@ const printToDom = (divId, textToPrint) => {
 
 const createProjectCards = (taco) => {
   let domString = '';
-  if (projects.available === true) {
-  for (let i = 0; i < taco.length; i++) {
-    domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+  for (let [i, element] of taco.entries()) {
+    if (taco[i].available === true) {
+    domString += `<div class="card my-2 h-200" style="width: 18rem;" id=${i}>
                     <div class="card-body">
-                      <h5 class="card-title">${taco[i].title}</h5>
-                      <img src=${taco[i].screenshot} class="card-img">
-                      <p class="card-description">${taco[i].description}</p>
-                      <p class="card-tech">${taco[i].technologiesUsed}</p>
-                      <a href="#" class="card-link">${taco[i].url}</a>
-                      <a href="#" class="card-link">${taco[i].githubUrl}</a>
+                      <h5 class="card-title text-center">${element.title}</h5>
+                      <p class="card-description">${element.description}</p>
+                      <p class="card-tech">${element.technologiesUsed}</p>
+                      <a href="#" class="card-link">${element.url}</a>
+                      <a href="#" class="card-link">${element.githubUrl}</a>
                     </div>
+                    <img src=${element.screenshot} class="card-img-bottom">
                   </div>`;
     }
   }
-  printToDom(#projectPage, printToDom);
+   
+  printToDom('#projectsPage', domString);
 }
+  
+
 
 const init = () => {
   createProjectCards(projects);
